93d233f7695c0fa23457b694997873970354e9a9
andreyhigher@gmail.com <andreyhigher@gmail.com>
2019-04-22 20:00:27 +0300

    Added restorecon support

--- termux-permission-fixer.py
+++ termux-permission-fixer.py
@@ -8,23 +8,42 @@
 
 from os import getuid, popen, chmod
 from re import findall
-from subprocess import 
+from subprocess import run, PIPE
+termux_path = '/data/data/com.termux'
+
+def restorecon_checker():
+    checker = run(['su', '-c', 'restorecon -RF '+termux_path], stdout=PIPE)
+    if checker.returncode:
+        return 0
+    else: 
+        return 1
 
 
-script_path = '/data/data/com.termux/files/usr/bin/termux-fix-permissions'
+def tmp_file_info_finder():
+    tmp_file_name = popen('mktemp').read()
+    file_info_getter = run(f'su -c /system/bin/ls -Z {tmp_file_name}'.split(), stdout=PIPE)
+    if file_info_getter.returncode:
+        return 0
+    return file_info_getter.stdout.read()
+
+
+script_path = 'termux_path/files/usr/bin/termux-fix-permissions'
 writer = open(script_path, 'w')
 uname = getuid()
 fix_user_part = f'''#! /system/bin/sh
-su -c chown -R {uname}:{uname} /data/data/com.termux/'''
-try:
-    tmp_file_name = popen('mktemp').read()
-    tmp_file_info = popen(f'su -c /system/bin/ls -Z {tmp_file_name}').read()
-    selinux_context = findall(r'u:object_r:app_data_file:s0:[^ ]+',
-                              tmp_file_info)[0]
-    fix_selinux_part = f'\nsu -c chcon -R {selinux_context} /data/data/com.termux/'
-    writer.write(fix_user_part + fix_selinux_part)
-except IndexError:
-    writer.write(fix_user_part)
+su -c chown -R {uname}:{uname} termux_path/'''
+restorecoc_command = f'\nsu -c restorecon -RF {termux_path}'
+if restorecon_checker():
+    writer.write(fix_user_part+restorecoc_command)
+else:
+    tmp_file_info = tmp_file_info_finder()
+    if not tmp_file_info:
+        writer.write(fix_user_part)
+    else:
+        selinux_context = findall(r'u:object_r:app_data_file:s0:[^ ]+',
+                                tmp_file_info)[0]
+        fix_selinux_part = f'\nsu -c chcon -R {selinux_context} {termux_path}/'
+        writer.write(fix_user_part + fix_selinux_part)
 writer.close()
 chmod(script_path, 0o700)
 print(f'Теперь вы, скорей всего, можете восстанавливать права для Termux, запустив файл {script_path}')
